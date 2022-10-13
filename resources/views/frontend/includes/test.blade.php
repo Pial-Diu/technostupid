50e12cdd4f024082ce17834c451302b125e52d73
Md. Mahady Hasan <pial.coder@gmail.com>
2022-10-14 03:57:12 +0600

    t

---
+++ resources/views/frontend/includes/test.blade.php
@@ -0,0 +1,49 @@
+<section class="contact py-5" id="about-me">
+      <div class="container">
+	  <!-- <h2 class="text-centre">Things I've built for the web</h2> -->
+        <div class="row">
+          
+        <div id="lightgallery">
+            <a href="/frontend/images/project/beedamall.png" data-lg-size="1600-2400">
+                <img alt="img1" src="/frontend/images/project/beedamall.png" />
+            </a>
+            <a href="/frontend/images/project/amujamu.png" data-lg-size="1024-800">
+                <img alt="img2" src="/frontend/images/project/amujamu.png" />
+            </a>
+            <a href="/frontend/images/project/vroom.png" data-lg-size="1024-800">
+                <img alt="img2" src="/frontend/images/project/vroom.png" />
+            </a>
+        </div>
+
+        </div>
+      </div>
+    </section>
+	<style>
+		 a, a:hover, a:focus, a:active {
+			text-decoration: none;
+			color: inherit;
+		}
+
+		.imgctr {
+			/* display: inline-block; */
+			/* margin: 20px; */
+			/* border: 1px solid black; */
+			border-radius: 10px;
+			overflow: hidden; 
+		}
+
+
+		#profile-pic {
+			overflow: hidden;
+			filter: sepia(30%);
+			transition: 0.5s all ease-in-out;
+			display: block;
+		}
+		#profile-pic:hover {
+			filter: sepia(0%);
+			/* transform:scale(1.5); */
+			transform: scale(1.1);
+  transform-origin: 50% 50%;
+		}
+		
+	</style>
